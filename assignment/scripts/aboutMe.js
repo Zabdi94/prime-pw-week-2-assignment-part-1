a2369819f7e1c23fc729b755f549f22532482d33
Zakariye Abdi <zak.abdi94@gmail.com>
2022-12-09 20:03:35 -0600

    added quotations to the script

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -1,31 +1,31 @@
 // REQUIRED FEATURES:
 // 1 - Create a variable called `firstName` and assign it the value of your first name
-let firstName = 'Zak';
+let firstName = 'Zakariye';
 // 2 - Create a second variable called `lastName` and assign it the value of your last name
 let lastName = 'Abdi';
 // 3 - Create a third variable called `fullName`, assign it the value of your first and last name
 // (remember, you already have variables for this, can you use those?)
-let fullName = firstName + lastName
+let fullName = 'Zakariye' + 'Abdi'
 // 4 - Console log the value of `fullName`
-console.log (fullName);
+console.log ('Zakariye Abdi');
 // 5 - Create a variable called `luckyNumber` and assign it the value of your lucky number.
-let luckyNumber = 18;
+let luckyNumber = '18';
 // 6 - Console log this sentence, adding in the variables you created above: 
 // 'My name is (full name), and I think (lucky number) is a winner!'.
 // Refer back to the videos if you need help with this one.
 console.log('My name is Zak Abdi, and I think 18 is a winner!') 
 // 7 - Create a variable named `adventurous` and set it to a boolean value (true or false)
-let adventurous = true;
+let adventurous = 'true';
 // 8 - Create a variable named `food`, and set its value to a string of your favorite food
-let food = pizza;
+let food = 'Pizza';
 // 9 - Create a variable called `pets` and set it to the value of the number of pets you have
-let pets = 0;
+let pets = '0';
 // 10 - Create a variable called `friendsPets` and assign it the value of the number of pets your friend has
-let friendsPets = 1;
+let friendsPets = '1';
 // 11 - Add two pets to your `pets` variable
-pets += 2;
+pets += '2';
 // 12 - Create a constant variable called `allowedPets` and set it to a number value of your choice
-let allowedPets = 4;
+let allowedPets = '4';
 // 13 - Create a conditional: if adventurous is true, console log "Adventures are great!", 
 // if it's not true, console log "How about we stay home?"
 if(adventurous) { console.log ('adventures are great');
